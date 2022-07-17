53b89c069a79305feb45cedc0d062f3f328da8de
Harshit Tiwari <harshitntiwari@gmail.com>
2022-07-17 17:51:54 +0530

    commit to main

--- application/app.py
+++ application/app.py
@@ -1,16 +1,42 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, join_room
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
-login_manager = LoginManager()
+from database import get_user
+
 app = Flask(__name__)
+app.secret_key = 'firlkjblkdbour'
 # Creating a socketio app object
 # it is encapsulating the flask app object
 socketio = SocketIO(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 @app.route('/')
 def home():
 	return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	message = ""
+	if request.method == 'POST':
+		username = request.form['username']
+		password_input = request.form['password']
+		user = get_user(username)
+
+		if user and user.check_password(password_input):
+			login_user(user)
+			return redirect(url_for('home'))
+		else:
+			message = 'Invalid Credentials. Failed to login!'
+	return render_template('login.html', message=message)
+
+@app.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	return redirect(url_for('home'))
+
 
 
 @app.route('/chat', methods = ['POST','GET'])
@@ -24,6 +50,8 @@ def chat():
 		return redirect(url_for('home'))
 
 
+# Server side communications
+
 @socketio.on('send_message')
 def handle_send_message_event(data):
 	app.logger.info("{} has sent the message to the room{}: {}".format(data['username'], data['room'], data['message']))
@@ -35,6 +63,10 @@ def handle_join_room_event(data):
 	app.logger.info("{} has joined the room {}".format(data['username'], data['room']))
 	join_room(data['room'])  # this will make that particular client join the room
 	socketio.emit('join_room_announcement', data)  #emitting event for announcement
+
+@login_manager.user_loader
+def load_user(username):
+	return get_user(username)
 
 # Running in debug mode
 if __name__=='__main__':
--- application/database.py
+++ application/database.py
@@ -1,6 +1,7 @@
 # contains all the logic to connct to the database.
 from pymongo import MongoClient
 from werkzeug.security import generate_password_hash
+from user import User
 
 client = MongoClient("mongodb+srv://harshit:1234@chatapp.6eoul.mongodb.net/?retryWrites=true&w=majority")
 
@@ -12,5 +13,8 @@ users_collection = chat_db.get_collection("users") # users is the name of the co
 def save_user_info(username, email, password):
 	password_hash = generate_password_hash(password)
 	users_collection.insert_one({'_id': username, 'email': email, 'password': password_hash})  #username is to be used as the primary key-its going to be unique
-
-save_user_info("abc","g@gh", "1234" )
+ 
+# to fetch data from the database
+def get_user(username):
+	user_data = users_collection.find_one({'_id': username})
+	return User(user_data['_id'], user_data['email'], user_data['password']) if user_data else None
